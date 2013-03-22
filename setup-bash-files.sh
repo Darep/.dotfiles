7933b1242b26f5de7bc2488e4d1fabcc6d61394a
ajk <ajk@ajk.fi>
2013-03-22 12:04:03 +0200

    Add a script for linking bash files easily

---
+++ setup-bash-files.sh
@@ -0,0 +1,11 @@
+cd ~
+ln -s .dotfiles/bash/.aliases
+ln -s .dotfiles/bash/.bash_login
+ln -s .dotfiles/bash/.bash_profile
+ln -s .dotfiles/bash/.bash_prompt
+ln -s .dotfiles/bash/.bashrc
+ln -s .dotfiles/bash/.extra
+ln -s .dotfiles/bash/.functions
+ln -s .dotfiles/bash/.inputrc
+ln -s .dotfiles/bash/.path
+ln -s .dotfiles/bash/.screenrc
